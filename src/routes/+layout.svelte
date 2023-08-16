ab6ced244209fb2a683885dc7415edc61e716af6
Nicolai <nicolaibernhardchristensen@gmail.com>
2023-08-16 21:01:00 +0200

    somewhat finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-  import "../app.css";
-  import Nav from "../components/nav.svelte"
+	import '../app.css';
+	import Nav from '../components/nav.svelte';
 </script>
 
-<div class="font-poppins bg-gradient-to-br from-amber-300/50 to-emerald-300/50 dark:from-slate-900 dark:to-stone-900 text-slate-950 transition-colors duration-500 dark:text-zinc-50 w-full min-h-screen">
-  <main class="max-w-[90%] w-full md:w-3/4 mx-auto py-8 min-h-screen flex flex-col">
-  <Nav/>
-  <slot />
-</main>
+<div
+	class="font-poppins bg-gradient-to-br from-amber-300/50 to-emerald-300/50 dark:from-slate-900 dark:to-stone-900 text-slate-950 transition-colors duration-500 dark:text-zinc-50 w-full min-h-screen"
+>
+	<main class="max-w-[90%] w-full md:w-3/4 mx-auto py-8 min-h-screen flex flex-col pb-32">
+		<Nav />
+		<slot />
+	</main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,158 @@
+<div class="flex-1 flex flex-col min-h-[795px]">
+	<div aria-label="page" class="flex-1 flex items-center">
+		<div class="flex flex-col items-start gap-4">
+			<h1 class="md:text-6xl text-5xl font-extrabold max-w-[80%] w-full">
+				Welcome! I am a software developer based in Scandinavia
+			</h1>
+			<p>
+				Checkout my <a
+					class="decoration-dotted underline underline-offset-2 decoration-[1px]"
+					href="https://github.com/barrybtw"
+					rel="noreferrer"
+					target="_blank">GitHub</a
+				>
+				or send me an email at
+				<a
+					href="mailto:barryxbtw@gmail.com"
+					class="decoration-dotted underline underline-offset-2 decoration-[1px]"
+					>barryxbtw@gmail.com</a
+				>
+			</p>
+		</div>
+	</div>
+</div>
+<div class="flex-1 flex flex-col min-h-[795px]">
+	<div aria-label="page" class="flex-1 flex items-start">
+		<div class="flex flex-col items-start gap-12">
+			<h1 class="md:text-5xl text-4xl font-bold w-full">These are my various projects!</h1>
+			<ul class="flex flex-col gap-32">
+				<li class="flex-1 flex flex-col gap-4">
+					<a
+						class="md:text-2xl text-xl underline decoration-dotted decoration-[1px] underline-offset-2"
+						href="https://github.com/barrybtw/aktiesparekonto"
+						rel="noreferrer"
+						target="_blank"
+						aria-label="stock calculator project"
+					>
+						Stock calculator
+					</a>
+					<div class="flex flex-row justify-between gap-6 lg:flex-col xl:flex-row">
+						<div class="flex flex-col">
+							<div aria-label="text-content" class="space-y-4 flex-1">
+								<p>
+									A project built with Next.js' app directory, the ui is put together with <a
+										href="https://www.radix-ui.com/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">radix</a
+									>
+									&
+									<a
+										href="https://tailwindcss.com/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">tailwindcss</a
+									>.
+								</p>
+								<p>
+									The calculations are based on a danish stock account, and said accounts specific
+									tax regulations.
+								</p>
+								<p>
+									You are able to input a specific amount of money, that you wish to deposit each
+									month, and however much you expect your account to rise on average each year.
+								</p>
+								<p>
+									The last option being how many years you expect this investment to be on it's feet
+									before you will cashout your supposed winnings.
+								</p>
+							</div>
+						</div>
 
-<div class="h-3/4 flex-1 flex">
-    <div aria-label="page" class="flex-1 flex items-center">
-      <div class="flex flex-col items-start gap-4">
+						<img
+							src="/aktiesparekonto.png"
+							alt="project"
+							class="rounded-md ring-1 w-1/2 hidden lg:block xl:w-1/2 lg:w-full"
+						/>
+					</div>
+				</li>
+				<li class="flex-1 flex flex-col gap-4">
+					<a
+						class="md:text-2xl text-xl underline decoration-dotted decoration-[1px] underline-offset-2"
+						href="https://github.com/barrybtw/create-drippy-api"
+						rel="noreferrer"
+						target="_blank"
+						aria-label="stock calculator project"
+					>
+						create-drippy-api
+					</a>
+					<div class="flex flex-row justify-between gap-6 lg:flex-col xl:flex-row">
+						<div class="flex flex-col">
+							<div aria-label="text-content" class="space-y-4 flex-1">
+								<p>
+									A command-line-interface, better known as a CLI built with <a
+										href="https://github.com/microsoft/TypeScript"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">typescript</a
+									>
+									&
+									<a
+										href="https://github.com/egoist/tsup"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">tsup</a
+									> to transpile and bundle the cli.
+								</p>
+								<p>
+									The CLI was made to scaffold a typescript based API based on <a
+										href="https://expressjs.com/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">Express.js</a
+									>, and to provide a good base layer for an API in regards to the configuration of
+									typescript and getting hot-module-reloading setup with
+									<a
+										href="https://nodemon.io/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">nodemon</a
+									>
+									&
+									<a
+										href="https://www.npmjs.com/package/ts-node"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">ts-node</a
+									>.
+								</p>
+								<p>
+									In the future I plan to add more options to the CLI, such as the ability to choose
+									a different framework, such as <a
+										href="https://fastify.dev/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">Fastify</a
+									>
+									or
+									<a
+										href="https://koajs.com/"
+										rel="noreferrer"
+										class="underline decoration-dotted decoration-[1px] underline-offset-2"
+										target="_blank">Koa</a
+									>.
+								</p>
+							</div>
+						</div>
 
-        <h1 class="text-5xl font-extrabold max-w-[60%] w-full">Welcome! I am a software developer based in Scandinavia</h1>
-        <p>Checkout my <a class="decoration-dotted underline underline-offset-2  decoration-[1px]" href="https://github.com/barrybtw" rel="noreferrer" target="_blank">GitHub</a></p>
-      </div>
-    </div>
+						<img
+							src="/drippy-api.png"
+							alt="project"
+							class="rounded-md ring-1 w-1/2 hidden lg:block xl:w-1/2 lg:w-full"
+						/>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
 </div>
